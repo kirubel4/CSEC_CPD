6a386d2c7e2290969e2401201318183d2d765544
kirubel4 <kirubelayele2127@gmail.com>
2025-02-08 19:04:13 +0300

    solution

--- Night at the Museum.py
+++ Night at the Museum.py
@@ -1,12 +1,9 @@
-exhibit_name = input()
-
-current_position = 'a'
-total_rotations = 0
-
-for letter in exhibit_name:
-    clockwise_distance = abs(ord(letter) - ord(current_position))
-    counterclockwise_distance = 26 - clockwise_distance
-    total_rotations += min(clockwise_distance, counterclockwise_distance)
-    current_position = letter  # Move the pointer to the new letter
-
-print(total_rotations)
+s = input()
+pos = 'a'
+rotations = 0
+for c in s:
+    cw = abs(ord(c) - ord(pos))
+    ccw = 26 - cw
+    rotations += min(cw, ccw)
+    pos = c  
+print(rotations)
